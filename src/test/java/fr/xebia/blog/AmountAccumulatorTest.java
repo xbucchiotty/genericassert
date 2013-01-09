dc1e5ca829200f4040da0dabdd4df3ea94f94148
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2013-01-09 19:37:38 +0100

    Refactor with GenericAssert

--- src/test/java/fr/xebia/blog/AmountAccumulatorTest.java
+++ src/test/java/fr/xebia/blog/AmountAccumulatorTest.java
@@ -6,25 +6,17 @@ import org.junit.Test;
 import java.math.BigDecimal;
 import java.util.Currency;
 
-import static java.lang.String.format;
-import static org.fest.assertions.Assertions.assertThat;
-
 public class AmountAccumulatorTest {
 
     public static final BigDecimal _5 = new BigDecimal("5");
     public static final BigDecimal _10 = new BigDecimal("10");
     public static final Currency USD = Currency.getInstance("USD");
 
-    private static final String ABSENCE_ERROR = "The amount shouldn't exists";
-    private static final String PRESENCE_ERROR = "The amount should exists";
-    private static final String CURRENCY_ERROR = "Currency of the amount";
-    private static final String VALUE_ERROR = "Value of the amount";
-
     @Test
     public void should_get_absent_for_empty_accumulator() {
         Optional<Amount> actualAmount = new AmountAccumulator().getAmount();
 
-        assertThat(actualAmount.isPresent()).overridingErrorMessage(ABSENCE_ERROR).isFalse();
+        OptionalAmountAsserter.assertThat(actualAmount).isAbsent();
     }
 
     @Test
@@ -37,16 +29,8 @@ public class AmountAccumulatorTest {
                 .getAmount();
 
         //PRESENCE OF AMOUNT
-        assertThat(actualAmount.isPresent()).overridingErrorMessage(PRESENCE_ERROR).isTrue();
-
-        //CURRENCY
-        Currency actualCurrency = actualAmount.get().getCurrency();
-        Currency expectedCurrency = USD;
-        assertThat(actualCurrency).as(CURRENCY_ERROR).isEqualTo(expectedCurrency);
-
-        //VALUE
-        BigDecimal actualValue = actualAmount.get().getValue();
-        BigDecimal expectedAmount = _10;
-        assertThat(actualValue).as(VALUE_ERROR).isEqualTo(expectedAmount);
+        OptionalAmountAsserter.assertThat(actualAmount)
+                .hasCurrency(USD)
+                .hasValue(_10);
     }
 }
---
+++ src/test/java/fr/xebia/blog/OptionalAmountAsserter.java
@@ -0,0 +1,61 @@
+package fr.xebia.blog;
+
+import com.google.common.base.Optional;
+import org.fest.assertions.Assertions;
+import org.fest.assertions.GenericAssert;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static java.lang.String.format;
+
+public class OptionalAmountAsserter extends GenericAssert<OptionalAmountAsserter, Optional<Amount>> {
+
+    private static final String NULL_ERROR = "The amount should not be null";
+    private static final String ABSENCE_ERROR = "The amount should exists";
+    private static final String PRESENCE_ERROR = "The amount should exists";
+    private static final String CURRENCY_ERROR = "Currency of the amount";
+    private static final String VALUE_ERROR = "Value of the amount";
+
+    OptionalAmountAsserter(Optional<Amount> actual) {
+        super(OptionalAmountAsserter.class, actual);
+    }
+
+    public static OptionalAmountAsserter assertThat(Optional<Amount> actual) {
+        return new OptionalAmountAsserter(actual);
+    }
+
+    public OptionalAmountAsserter isAbsent() {
+        Assertions.assertThat(actual).overridingErrorMessage(NULL_ERROR).isNotNull();
+        Assertions.assertThat(actual.isPresent()).overridingErrorMessage(ABSENCE_ERROR).isFalse();
+
+        return this;
+    }
+
+    public OptionalAmountAsserter isPresent() {
+        Assertions.assertThat(actual).overridingErrorMessage(NULL_ERROR).isNotNull();
+        Assertions.assertThat(actual.isPresent()).overridingErrorMessage(PRESENCE_ERROR).isTrue();
+
+        return this;
+    }
+
+    public OptionalAmountAsserter hasCurrency(Currency expected) {
+        Assertions.assertThat(actual).overridingErrorMessage(NULL_ERROR).isNotNull();
+        isPresent();
+        Currency actualCurrency = actual.get().getCurrency();
+
+        Assertions.assertThat(actualCurrency).describedAs(CURRENCY_ERROR).isEqualTo(expected);
+
+        return this;
+    }
+
+    public OptionalAmountAsserter hasValue(BigDecimal expected) {
+        Assertions.assertThat(actual).overridingErrorMessage(NULL_ERROR).isNotNull();
+        isPresent();
+        BigDecimal actualValue = actual.get().getValue();
+
+        Assertions.assertThat(actualValue).describedAs(VALUE_ERROR).isEqualTo(expected);
+
+        return this;
+    }
+}
