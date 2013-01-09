15fdd34eb54e3a48b7731b16922adeadbf5588eb
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2013-01-09 19:34:17 +0100

    Add the sum of amounts for the second use case of accumulator

--- src/main/java/fr/xebia/blog/AmountAccumulator.java
+++ src/main/java/fr/xebia/blog/AmountAccumulator.java
@@ -3,10 +3,35 @@ package fr.xebia.blog;
 import com.google.common.base.Optional;
 
 import static com.google.common.base.Optional.absent;
+import static com.google.common.base.Optional.of;
 
 public class AmountAccumulator {
 
-    public Optional<Amount> getAmount(){
-        return absent();
+    private final Optional<Amount> amount;
+
+    public AmountAccumulator() {
+        this.amount = absent();
+    }
+
+    public AmountAccumulator(Amount accumulate) {
+        this.amount = of(accumulate);
+    }
+
+    public AmountAccumulator accumulate(final Amount amount) {
+        AmountAccumulator nextState;
+
+        if (!this.amount.isPresent()) {
+            nextState = new AmountAccumulator(amount);
+        } else {
+            Amount currentState = this.amount.get();
+
+            nextState = new AmountAccumulator(currentState.add(amount.getValue()));
+        }
+
+        return nextState;
+    }
+
+    public Optional<Amount> getAmount() {
+        return amount;
     }
 }
--- src/test/java/fr/xebia/blog/AmountAccumulatorTest.java
+++ src/test/java/fr/xebia/blog/AmountAccumulatorTest.java
@@ -3,14 +3,35 @@ package fr.xebia.blog;
 import com.google.common.base.Optional;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
 import static org.fest.assertions.Assertions.assertThat;
 
 public class AmountAccumulatorTest {
+
+    public static final BigDecimal _5 = new BigDecimal("5");
+    public static final BigDecimal _10 = new BigDecimal("10");
+    public static final Currency USD = Currency.getInstance("USD");
 
     @Test
     public void should_get_absent_for_empty_accumulator() {
         Optional<Amount> actualAmount = new AmountAccumulator().getAmount();
 
         assertThat(actualAmount.isPresent()).isFalse();
+    }
+
+    @Test
+    public void should_get_10_USD_when_accumulate_5_USD_2_times() {
+        Amount _5_USD = new Amount(_5, USD);
+
+        Optional<Amount> actualAmount = new AmountAccumulator()
+                .accumulate(_5_USD)
+                .accumulate(_5_USD)
+                .getAmount();
+
+        assertThat(actualAmount.isPresent()).isTrue();
+        assertThat(actualAmount.get().getCurrency()).isEqualTo(USD);
+        assertThat(actualAmount.get().getValue()).isEqualTo(_10);
     }
 }
