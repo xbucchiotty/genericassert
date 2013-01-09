d7e49e792885697893c3315607874f43633da1f9
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2013-01-09 19:35:53 +0100

    add custom error message

--- src/test/java/fr/xebia/blog/AmountAccumulatorTest.java
+++ src/test/java/fr/xebia/blog/AmountAccumulatorTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.math.BigDecimal;
 import java.util.Currency;
 
+import static java.lang.String.format;
 import static org.fest.assertions.Assertions.assertThat;
 
 public class AmountAccumulatorTest {
@@ -14,11 +15,16 @@ public class AmountAccumulatorTest {
     public static final BigDecimal _10 = new BigDecimal("10");
     public static final Currency USD = Currency.getInstance("USD");
 
+    private static final String ABSENCE_ERROR = "The amount shouldn't exists";
+    private static final String PRESENCE_ERROR = "The amount should exists";
+    private static final String CURRENCY_ERROR = "Currency of the amount";
+    private static final String VALUE_ERROR = "Value of the amount";
+
     @Test
     public void should_get_absent_for_empty_accumulator() {
         Optional<Amount> actualAmount = new AmountAccumulator().getAmount();
 
-        assertThat(actualAmount.isPresent()).isFalse();
+        assertThat(actualAmount.isPresent()).overridingErrorMessage(ABSENCE_ERROR).isFalse();
     }
 
     @Test
@@ -30,8 +36,17 @@ public class AmountAccumulatorTest {
                 .accumulate(_5_USD)
                 .getAmount();
 
-        assertThat(actualAmount.isPresent()).isTrue();
-        assertThat(actualAmount.get().getCurrency()).isEqualTo(USD);
-        assertThat(actualAmount.get().getValue()).isEqualTo(_10);
+        //PRESENCE OF AMOUNT
+        assertThat(actualAmount.isPresent()).overridingErrorMessage(PRESENCE_ERROR).isTrue();
+
+        //CURRENCY
+        Currency actualCurrency = actualAmount.get().getCurrency();
+        Currency expectedCurrency = USD;
+        assertThat(actualCurrency).as(CURRENCY_ERROR).isEqualTo(expectedCurrency);
+
+        //VALUE
+        BigDecimal actualValue = actualAmount.get().getValue();
+        BigDecimal expectedAmount = _10;
+        assertThat(actualValue).as(VALUE_ERROR).isEqualTo(expectedAmount);
     }
 }
