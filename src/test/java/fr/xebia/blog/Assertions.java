c9736a212c3805c13d7fe7442d53a98a5b55a4b3
Joel Costigliola <joel.costigliola@gmail.com>
2013-02-04 19:25:55 +0100

    Use assertions generator and fest assert core 2.0M8.

--- src/test/java/fr/xebia/blog/Assertions.java
+++ src/test/java/fr/xebia/blog/Assertions.java
@@ -2,9 +2,9 @@ package fr.xebia.blog;
 
 import com.google.common.base.Optional;
 
-public class Assertions extends org.fest.assertions.Assertions{
+public class Assertions extends org.fest.assertions.api.Assertions {
 
-    public static OptionalAmountAsserter assertThat(Optional<Amount> actual) {
-        return new OptionalAmountAsserter(actual);
-    }
+  public static OptionalAmountAsserter assertThat(Optional<Amount> actual) {
+    return new OptionalAmountAsserter(actual);
+  }
 }
